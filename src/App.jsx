70b11ad1032eb969ea543f804fbfdcea24924a4a
Toto5931 <antoine.abele@gmail.com>
2025-08-05 21:34:26 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <h1>Hello Capacitor + React + Vite</h1>
+    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
+    <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
+    <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.                                                             
+    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.                                                             
+    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.                                                             
+    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
+</>
+  );
+
+}
+
+export default App
+      
