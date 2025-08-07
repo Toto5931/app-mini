1e435d534835a0db97838eb3b3cceb958e7e002c
Toto5931 <antoine.abele@gmail.com>
2025-08-07 19:05:47 +0200

    putting all the content in a div

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <>
+    <div>
     <h1>Hello Capacitor + React + Vite</h1>
     <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
     <button onClick={() => alert('Button clicked!')}>Click Me</button>
@@ -15,7 +15,7 @@ function App() {
     <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.                                                             
     <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
-</>
+</div>
   );
 
 }
