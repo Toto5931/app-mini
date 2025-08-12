df0ce03bb61aad04e075481fc818edede8bb5612
Toto5931 <antoine.abele@gmail.com>
2025-08-12 16:32:18 +0200

    reset ios folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,13 @@ function App() {
   return (
     <div>
     <h1>Hello Capacitor + React + Vite</h1>
-    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
-    <button onClick={() => alert('Button clicked!')}>Click Me</button>
-    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
-    <button onClick={() => alert('Button clicked!')}>Click Me</button>
-    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.                                                             
-    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.                                                             
-    <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.                                                             
+
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.   </div>                                                          
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.         </div>                                                    
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.      </div>                                                         
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.   </div>                                                          
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.         </div>                                                    
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quam earum, beatae dolorem amet. Porro quam quas, hic iure est officia, laborum ipsa perferendis laudantium aut perspiciatis velit atque.      </div>      
     <div style={{margin:"40px"}}><input type="text" placeholder="Type here..." /></div>
 </div>
   );
