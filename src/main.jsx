1b3bcde8fb438ee296bb8927c92d143a2ed14ddd
Toto5931 <antoine.abele@gmail.com>
2025-08-12 22:30:32 +0200

    other try safe area

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
-import '@capacitor-community/safe-area';
+import { SafeArea } from '@capacitor-community/safe-area';
+
+SafeArea.enable({
+  config: {
+    customColorsForSystemBars: true,
+    statusBarColor: '#00000000', // transparent
+    statusBarContent: 'light',
+    navigationBarColor: '#00000000', // transparent
+    navigationBarContent: 'light',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
